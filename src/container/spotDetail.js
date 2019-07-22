08c68685335e12dd8e028620d6b241c1df0bf4a4
yinmin <alice0050722@gmail.com>
2019-07-22 09:50:40 +0800

    add swiper on spotDetail

--- src/container/spotDetail.js
+++ src/container/spotDetail.js
@@ -4,32 +4,26 @@ import {
   Text,
   Image,
   StyleSheet,
-  Dimensions
+  Dimensions,
 } from 'react-native'
 import Swiper from 'react-native-swiper'
+import Images from '../config/images'
 
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1
   },
-  swiper: {
-    borderColor: 'red',
-    borderWidth: 5,
-    flexDirection: 'row',
-    height: Dimensions.get('window').height * 0.3,
-    justifyContent: 'space-evenly'
+  hr: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  },
+  wrapper: {
   },
   slide: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // width: Dimensions.get('window').width,
-    // height: Dimensions.get('window').height * 0.3
-  },
-  hr: {
-    borderBottomColor: 'black',
-    borderBottomWidth: 1
   }
 })
 
@@ -45,15 +39,22 @@ export default class SpotDetail extends React.Component {
       <View style={styles.container}>
         <Text>實景照片</Text>
         <View style={styles.hr} />
-        <Swiper style={styles.container} showsButtons={true} autoplay={true}>
-          <Image style={styles.slide} source={require('../assets/1.png')} />
-          <Image style={styles.slide} source={require('../assets/1.png')} />
-          <Image style={styles.slide} source={require('../assets/1.png')} />
+        <Swiper style={styles.wrapper} showsButtons autoplay>
+          <Image style={styles.slide} source={Images.recImg} />
+          <Image style={styles.slide} source={Images.recImg} />
+          <Image style={styles.slide} source={Images.recImg} />
+          <Image style={styles.slide} source={Images.recImg} />
+          <Image style={styles.slide} source={Images.recImg} />
         </Swiper>
-        <Text>潛點介紹</Text>
-        <View style={styles.hr} />
-        <Text>潛點推薦</Text>
-        <View style={styles.hr} />
+        <View style={{flex:1}}>
+          <Text>潛點介紹</Text>
+          <View style={styles.hr} />
+
+        </View>
+        <View style={{flex:1}}>
+          <Text>潛點推薦</Text>
+          <View style={styles.hr} />
+        </View>
       </View>
     )
   }
