b728231e5c857c6886dce777b6c1d19f2fbf2c61
yinmin <alice0050722@gmail.com>
2019-07-23 15:24:17 +0800

    add style

--- src/config/style.js
+++ src/config/style.js
@@ -2,7 +2,19 @@ import {
   StyleSheet,
 } from 'react-native';
 
+import {
+  Colors
+} from '../config';
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lightGray,
+    fontSize: 15,
+    color: Colors.gray,
+    fontFamily: "Noto Sans CJK TC"
+  }
+
 });
 
 export default styles;
