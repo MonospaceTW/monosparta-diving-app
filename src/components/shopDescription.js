b5b41952a73aeb956767805464cb72ac0664d672
Shuen-Shiang Yang <brian19960@gmail.com>
2019-07-30 16:19:52 +0800

    add modal component

--- src/components/shopDescription.js
+++ src/components/shopDescription.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native'
 
 import Styles from '../config/style'
 
+const styles = StyleSheet.create({
+  txt: {
+    fontSize: 15,
+    lineHeight: 20
+  },
+})
 
 export default class ShopDescription extends React.Component {
 
@@ -15,7 +22,7 @@ export default class ShopDescription extends React.Component {
       <View>
           <Text style={Styles.title}>{this.props.name}</Text>
         <View style={Styles.content}>
-          <Text>{this.props.description}</Text>
+          <Text style={styles.txt}>{this.props.description}</Text>
         </View>
       </View>
     );
